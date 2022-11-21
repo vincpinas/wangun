6b7e37a2c4458fc7d4762b627eb12c9b4ebba02f
duetvincent <vincent@duet.digital>
2022-11-21 19:53:47 +0100

    :package: changed composer for optimization

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,17 @@
-import { Vector2 } from 'three';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
-import { PixelShader } from 'three/examples/jsm/shaders/PixelShader';
-import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
+import { EffectComposer, RenderPass, EffectPass, BloomEffect, PixelationEffect, FXAAEffect } from 'postprocessing';
 import Game from './Classes/Game';
 
 // Game object, this stores all the objects the game will use apart of the post processing unit.
 const game = new Game();
 
-
 // Post processing/rendering unit /*
 const composer = new EffectComposer(game.renderer);
 // Pass renderer to composer.
 composer.addPass(new RenderPass(game.scene, game.camera));
-// Bloom filter.
-const bloomPass = new UnrealBloomPass(new Vector2(window.innerWidth, window.innerHeight), 1.5, 0.7, 0.85);
-composer.addPass(bloomPass);
-// Pixel filter.
-const pixelPass = new ShaderPass(PixelShader);
-pixelPass.uniforms['resolution'].value = new Vector2(window.innerWidth, window.innerHeight);
-pixelPass.uniforms['resolution'].value.multiplyScalar(window.devicePixelRatio);
-pixelPass.uniforms['pixelSize'].value = game.config.pixelRatio;
-composer.addPass(pixelPass);
-// FXAA filter.
-const fxaaPass = new ShaderPass(FXAAShader);
-fxaaPass.material.uniforms['resolution'].value.x = 1 / (window.innerWidth * game.renderer.getPixelRatio());
-fxaaPass.material.uniforms['resolution'].value.y = 1 / (window.innerHeight * game.renderer.getPixelRatio());
-composer.addPass(fxaaPass);
+// Post processing effects.
+composer.addPass(new EffectPass(game.camera, new BloomEffect()))
+composer.addPass(new EffectPass(game.camera, new PixelationEffect(game.config.pixelRatio)))
+composer.addPass(new EffectPass(game.camera, new FXAAEffect()))
 
 
 // On window resizing.
@@ -37,7 +20,6 @@ window.onresize = () => {
   game.camera.updateProjectionMatrix();
 
   game.renderer.setSize(window.innerWidth, window.innerHeight);
-  pixelPass.uniforms['resolution'].value.set(window.innerWidth, window.innerHeight).multiplyScalar(window.devicePixelRatio);
 }
 
 
