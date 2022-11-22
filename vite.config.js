e96359d4d1c3d8e752e6edf9abe07f8e2fdd9193
duetvincent <vincent@duet.digital>
2022-11-22 11:23:20 +0100

    :gear: custom domain for dev environment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
 import liveReload from 'vite-plugin-live-reload'
+import fs from 'fs';
+
 
 export default defineConfig({
   server: {
     hmr: true,
+    open: true,
+    port: 3000,
+    https: {
+      key: fs.readFileSync('https/wangun.dev.key'),
+      cert: fs.readFileSync('https/wangun.dev.crt')
+    },
+    host: 'wangun.dev'
   },
   plugins: [
-    liveReload(['src/Scenes/*.ts', 'src/Classes/*.ts', 'src/*.ts',])
+    liveReload(['src/Scenes/*.ts', 'src/Classes/*.ts', 'src/*.ts',]),
   ],
-  base: "./",
-  assetsInclude: "**/*glb",
+  base: './',
+  assetsInclude: '**/*glb',
   build: {
     rollupOptions: {
       output: {
